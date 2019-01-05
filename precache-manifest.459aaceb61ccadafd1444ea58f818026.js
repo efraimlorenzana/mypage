fcad5d6f05bc10e51fd23e229408b2963b6665f6
efraim a lorenzana <efraim.lorenzana@gmail.com>
2019-01-05 23:00:52 +0800

    Updates

---
+++ precache-manifest.459aaceb61ccadafd1444ea58f818026.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cbdaf68f162c6a2b7f3f523f4f867fb",
+    "url": "/my-page/static/media/gradient.7cbdaf68.png"
+  },
+  {
+    "revision": "6e0566607e36b2d83cbf",
+    "url": "/my-page/static/css/main.889a6661.chunk.css"
+  },
+  {
+    "revision": "2a9f56579425823b5955",
+    "url": "/my-page/static/js/1.2a9f5657.chunk.js"
+  },
+  {
+    "revision": "d65eeecbc6dae33d2a8a",
+    "url": "/my-page/static/js/runtime~main.d65eeecb.js"
+  },
+  {
+    "revision": "1fc71700a5d0d7405a2bc5d7275ae637",
+    "url": "/my-page/static/media/sprite.1fc71700.svg"
+  },
+  {
+    "revision": "5cc61138ecc0f4a7b64de822f3180999",
+    "url": "/my-page/static/media/main.5cc61138.jpg"
+  },
+  {
+    "revision": "b7f7db007313cc1e3917970622cce88e",
+    "url": "/my-page/static/media/dreamlife.b7f7db00.png"
+  },
+  {
+    "revision": "0e5620ee7f46fc269b7dbf0e2a885aa4",
+    "url": "/my-page/static/media/easymarket.0e5620ee.jpg"
+  },
+  {
+    "revision": "6e0566607e36b2d83cbf",
+    "url": "/my-page/static/js/main.6e056660.chunk.js"
+  },
+  {
+    "revision": "9d33c4be44a771a6f21225900edbdd60",
+    "url": "/my-page/static/media/hr-fms.9d33c4be.jpg"
+  },
+  {
+    "revision": "2bd28a37711dca81e6464d60d8d1ff7f",
+    "url": "/my-page/static/media/numtoword.2bd28a37.jpg"
+  },
+  {
+    "revision": "226bd5281ab8794049507af6b5809d7d",
+    "url": "/my-page/static/media/robot-friends.226bd528.png"
+  },
+  {
+    "revision": "34fe82d18d601a2788c7574258bf9ff9",
+    "url": "/my-page/static/media/smart-brain.34fe82d1.png"
+  },
+  {
+    "revision": "2a684c5c52a2e28028aceabeeb604e61",
+    "url": "/my-page/static/media/cert1.2a684c5c.png"
+  },
+  {
+    "revision": "f5b308f601511c0dab63dc0f71f54041",
+    "url": "/my-page/static/media/cert2.f5b308f6.png"
+  },
+  {
+    "revision": "ee9d833484814ece65042fa2bf781ef9",
+    "url": "/my-page/index.html"
+  }
+];
