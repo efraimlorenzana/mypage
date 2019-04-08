4a9d8d0af0e993a2ebfca419ba2a587b59acb590
efraim a lorenzana <efraim.lorenzana@gmail.com>
2019-04-08 09:42:44 +0800

    add permanent profile picture

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { bio } from '../Data/personalInfo';
+import Me from '../Data/Images/me.jpg';
 
 class Main extends Component {
 
@@ -42,7 +43,7 @@ class Main extends Component {
                     </h1>
 
                     <div className="main__picture">
-                        <img src="https://scontent.fmnl3-2.fna.fbcdn.net/v/t1.0-9/42889876_2800033166688815_6600471337804759040_n.jpg?_nc_cat=110&_nc_eui2=AeH5qXW73i_uKUOc6MXmVYhCErwihZBxuBzaxUEJiaH1m11sTYHbT69qjylTs8H4EwScoh2jzuY3_gKA4IxWxNRnOgFPgMPiSBBICP09rfNifOGu-N8W2WOZiBautVjqcg8&_nc_ht=scontent.fmnl3-2.fna&oh=dedff30680d5dc555f9c34cea2c0e33f&oe=5C9A24A0" 
+                        <img src={Me} 
                         alt="Cover" 
                         className="main__picture--photo"
                         />
